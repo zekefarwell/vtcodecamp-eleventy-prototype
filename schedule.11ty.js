ef628c6bc482d81160554711db6b3ccc078a6cb7
Zeke Farwell <ezekielf@gmail.com>
2019-04-07 16:58:15 -0400

    extract layout.  build schedule table

--- schedule.11ty.js
+++ schedule.11ty.js
@@ -1,25 +1,58 @@
-class Test {
-    
+"use strict";
+
+class Schedule
+{
+
+    data() {
+        return {
+            layout: "page.njk",
+            title: "Schedule"
+        };
+    }
+
+
     render(data) 
     {
         var schedule = this.buildSchedule(data);
 
-        return "<pre>" + JSON.stringify(schedule) + "</pre>";
+        // return "<pre>" + JSON.stringify(schedule) + "</pre>";
+
+        let tableContent = "<table>"; 
+
+        // tableContent = `
+        //     <thead>
+        //         <tr>
+        //             <th>Time</th>
+        //             <th>${firstRowData.}</th>
+        //         </tr>
+        //     </thead>
+
+        // `;
+
+        tableContent += "<tbody>"; 
+        for (let [timeCode, rowSessions] of Object.entries(schedule)) {
+            let time = data.times[timeCode];
+            tableContent += "<tr>";
+            tableContent += `<th> ${time.start} - ${time.end} </th>`;
+            for (let session of Object.values(rowSessions)) {
+                tableContent += "<td>";
+                tableContent += `<a href="#" class="td-title">${session.title}<a>`;
+                tableContent += `<a href="#" class="td-subtitle">${session.speakers[0]}<a>`;
+                tableContent += "</td>";
+            }
+            tableContent += "</tr>";
+        } 
+        tableContent += "</tbody>"; 
+        tableContent += "</table>"
+
+        return tableContent;
+
     }
 
     buildSchedule(data)
     {
-        var header = [
-            { 'title': 'Time', 'subtitle': '' }
-        ];
-        for (var track of Object.values(data.tracks)) {
-            header.push({
-                'title': track.title,
-                'subtitle': data.spaces[track.space].title
-            });
-        }
+        let schedule = {};
 
-        var schedule = {};
         for (let session of Object.values(data.sessions)) {
             let time = session.time;
             let space = session.space;
@@ -28,13 +61,22 @@ class Test {
             }
             schedule[time][space] = session;
         }
+        return schedule;
+    }
 
-        return {
-            'header': header,
-            'schedule': schedule
-        };
+    renderHeader(row)
+    {
+        header = `
+            <thead> 
+                <tr>
+                    <th></th>
+                </tr>
+            </thead>
+        `
+
+        return row;
     }
 
 }
 
-module.exports = Test;
+module.exports = Schedule;
