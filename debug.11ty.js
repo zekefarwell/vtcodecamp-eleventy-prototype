24d2f03107702af35ed1990e5ae8dc7d80a71083
Zeke Farwell <ezekielf@gmail.com>
2019-04-07 23:04:28 -0400

    change build dir to /docs for github pages deploy

--- debug.11ty.js
+++ debug.11ty.js
@@ -1,5 +1,14 @@
 "use strict";
 
 module.exports = function(data) {
-  return JSON.stringify(data.sessionsByTrack);
+    return JSON.stringify({
+        levels: data.levels,
+        schedule: data.schedule,
+        sessions: data.sessions,
+        sessionsByTrack: data.sessionsByTrack,
+        spaces: data.spaces,
+        speakers: data.speakers,
+        times: data.times,
+        tracks: data.tracks,
+    });
 };
